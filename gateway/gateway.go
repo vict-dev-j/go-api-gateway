b595d165ba7f92ce5fa4f5c78e4ced0852be4999
Victoria Naumenko <vika.noumenko@gmail.com>
2024-06-19 13:30:43 +0300

    added jwt parsing

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,27 +1,101 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	_ "strings"
+	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
 var (
 	CustomersURL      = "http://localhost:8080"
 	InvestAccountsURL = "http://localhost:8082"
+	jwtSecret         = []byte("MY_SECRET_123")
 )
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/{service:customer|invest-account}{rest:.*}", Handler)
+	router.HandleFunc("/login", LoginHandler).Methods("POST")
+	router.HandleFunc("/{service:customer|invest-account}{rest:.*}", JWTMiddleware(Handler)).Methods("GET", "POST", "PUT", "DELETE")
 	fmt.Println("Gateway listening on :8081")
 	err := http.ListenAndServe(":8081", router)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: validate creds from database
+	if creds.Username != "admin" || creds.Password != "password" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		http.Error(w, "Error signing token", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+}
+
+func JWTMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := extractToken(r)
+		if tokenString == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if !token.Valid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -55,10 +129,10 @@ func proxyRequest(w http.ResponseWriter, r *http.Request, targetURL string) {
 
 	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Printf("Error proxying request to %s: %s\n", targetURL, err.Error())
 		http.Error(w, "Error proxying request", http.StatusBadGateway)
 		return
 	}
-	defer resp.Body.Close()
 
 	copyHeaders(w.Header(), resp.Header)
 	w.WriteHeader(resp.StatusCode)
@@ -75,4 +149,12 @@ func copyHeaders(dst, src http.Header) {
 			dst.Add(key, value)
 		}
 	}
+}
+
+func extractToken(r *http.Request) string {
+	bearerToken := r.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
 }
